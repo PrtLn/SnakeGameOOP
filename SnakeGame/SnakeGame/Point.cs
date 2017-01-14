9a278e5eb67ed80d1dd0c8605cd854742c72a733
oleno <pirit.lena@gmail.com>
2017-01-14 23:19:16 +0200

    creating objects and method of Point class

--- SnakeGame/SnakeGame/Point.cs
+++ SnakeGame/SnakeGame/Point.cs
@@ -11,5 +11,11 @@ namespace SnakeGame
         public int x;
         public int y;
         public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
