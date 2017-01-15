5bf8ac28227ef150e8d96fdf30572eda81a8db38
oleno <pirit.lena@gmail.com>
2017-01-15 17:10:12 +0200

    creating HorizontalLine class that draws horizontal line

---
+++ SnakeGame/SnakeGame/HorizontalLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine()
+        {
+            pList = new List<Point>();
+            Point p1 = new Point(4, 4, '*');
+            Point p2 = new Point(5, 4, '*');
+            Point p3 = new Point(6, 4, '*');
+            Point p4 = new Point(7, 4, '*');
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+        }
+
+        public void Drow()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+
+    }
+}
--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -16,32 +16,8 @@ namespace SnakeGame
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(3, 7, '^');
-            p3.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int a = numList[0];
-            int b = numList[1];
-            int c = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            Console.WriteLine("p1: x = " +p1.x + ", y = " + p1.y + ", sym = " + p1.sym);
-            pList.Add(p2);
-            Console.WriteLine("p2: x = " + p2.x + ", y = " + p2.y + ", sym = " + p2.sym);
-            pList.Add(p3);
-            Console.WriteLine("p3: x = " + p3.x + ", y = " + p3.y + ", sym = " + p3.sym);
+            HorizontalLine line = new HorizontalLine();
+            line.Drow();
             
             Console.ReadLine();
         }        
